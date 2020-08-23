a6329a4a743f81a13ed3292d5013acd76fab6d44
pamik <pamik@list.ru>
2020-08-23 22:29:31 +0300

    git all

--- paramoko.py
+++ paramoko.py
@@ -4,10 +4,17 @@ a = "172.17.54.108"
 cli = paramiko.client.SSHClient()
 cli.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
 cli.connect(hostname=a, username="pamik01", password="fQAg7MdHc01")
-stdin_, stdout_, stderr_ = cli.exec_command("cd bw/ && ls -lh")
-# time.sleep(2)    # Previously, I had to sleep for some time.
+stdin_, stdout_, stderr_ = cli.exec_command("cd bw/ && git rev-parse --abbrev-ref HEAD")
 stdout_.channel.recv_exit_status()
 lines = stdout_.readlines()
 for line in lines:
     print(line)
+    b = line
+stdin_, stdout_, stderr_ = cli.exec_command("cd bw/ && git rev-list --count HEAD")
+stdout_.channel.recv_exit_status()
+lines = stdout_.readlines()
+for line in lines:
+    print(line)
+    c = line
+print(b, c)
 cli.close()
